9c476cfc8c3d3b694c51b17ae2201cd6aac6d511
AdrianGerman <adrian.german1019@gmail.com>
2024-08-10 09:58:13 -0600

    config phaser and loading images and sprites

--- game.js
+++ game.js
@@ -1,1 +1,39 @@
-console.log("Hello World");
+const config = {
+  type: Phaser.AUTO,
+  width: 256,
+  height: 244,
+  backgroundColor: "#049cd8",
+  parent: "game",
+  scene: {
+    preload,
+    create,
+    update,
+  },
+};
+new Phaser.Game(config);
+
+function preload() {
+  // mapa
+  this.load.image("cloud1", "assets/scenery/overworld/cloud1.png");
+  this.load.image("floorbricks", "assets/scenery/overworld/floorbricks.png");
+
+  //personaje
+  this.load.spritesheet("mario", "assets/entities/mario.png", {
+    frameWidth: 18,
+    frameHeight: 16,
+  });
+}
+
+function create() {
+  // mapa
+  this.add.image(100, 50, "cloud1").setOrigin(0, 0).setScale(0.15);
+  this.add
+    .tileSprite(0, config.height, config.width, 32, "floorbricks")
+    .setScale(1)
+    .setOrigin(0, 1);
+
+  // personaje
+  this.add.sprite(50, 210, "mario").setOrigin(0, 1);
+}
+
+function update() {}
