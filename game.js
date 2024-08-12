483a1e2f539614c09d6fae79c4d28cea9f39fc25
AdrianGerman <adrian.german1019@gmail.com>
2024-08-12 08:22:51 -0600

    install standard and fortmat code

--- game.js
+++ game.js
@@ -1,111 +1,113 @@
-import { createAnimations } from "./animations.js";
+/* eslint-disable no-new */
+/* eslint-disable no-undef */
+import { createAnimations } from './animations.js'
 
 const config = {
   type: Phaser.AUTO,
   width: 256,
   height: 244,
-  backgroundColor: "#049cd8",
-  parent: "game",
+  backgroundColor: '#049cd8',
+  parent: 'game',
   physics: {
-    default: "arcade",
+    default: 'arcade',
     arcade: {
       gravity: { y: 300 },
-      debug: false,
-    },
+      debug: false
+    }
   },
   scene: {
     preload,
     create,
-    update,
-  },
-};
-new Phaser.Game(config);
+    update
+  }
+}
+new Phaser.Game(config)
 
-function preload() {
+function preload () {
   // mapa
-  this.load.image("cloud1", "assets/scenery/overworld/cloud1.png");
-  this.load.image("floorbricks", "assets/scenery/overworld/floorbricks.png");
+  this.load.image('cloud1', 'assets/scenery/overworld/cloud1.png')
+  this.load.image('floorbricks', 'assets/scenery/overworld/floorbricks.png')
 
-  //personaje
-  this.load.spritesheet("mario", "assets/entities/mario.png", {
+  // personaje
+  this.load.spritesheet('mario', 'assets/entities/mario.png', {
     frameWidth: 18,
-    frameHeight: 16,
-  });
+    frameHeight: 16
+  })
 
-  this.load.audio("gameover", "assets/sound/music/gameover.mp3");
+  this.load.audio('gameover', 'assets/sound/music/gameover.mp3')
 }
 
-function create() {
+function create () {
   // mapa
-  this.add.image(100, 50, "cloud1").setOrigin(0, 0).setScale(0.15);
-  this.floor = this.physics.add.staticGroup();
+  this.add.image(100, 50, 'cloud1').setOrigin(0, 0).setScale(0.15)
+  this.floor = this.physics.add.staticGroup()
   this.floor
-    .create(0, config.height - 16, "floorbricks")
+    .create(0, config.height - 16, 'floorbricks')
     .setOrigin(0, 0.5)
-    .refreshBody();
+    .refreshBody()
   this.floor
-    .create(150, config.height - 16, "floorbricks")
+    .create(150, config.height - 16, 'floorbricks')
     .setOrigin(0, 0.5)
-    .refreshBody();
+    .refreshBody()
 
   // personaje
   // this.mario = this.add.sprite(50, 210, "mario").setOrigin(0, 1);
 
   this.mario = this.physics.add
-    .sprite(50, 100, "mario")
+    .sprite(50, 100, 'mario')
     .setOrigin(0, 1)
     .setCollideWorldBounds(true)
-    .setGravityY(500);
+    .setGravityY(500)
 
-  this.physics.world.setBounds(0, 0, 2000, config.height);
-  this.physics.add.collider(this.mario, this.floor);
+  this.physics.world.setBounds(0, 0, 2000, config.height)
+  this.physics.add.collider(this.mario, this.floor)
 
-  this.cameras.main.setBounds(0, 0, 2000, config.height);
-  this.cameras.main.startFollow(this.mario);
+  this.cameras.main.setBounds(0, 0, 2000, config.height)
+  this.cameras.main.startFollow(this.mario)
 
-  createAnimations(this);
+  createAnimations(this)
 
-  this.keys = this.input.keyboard.createCursorKeys();
+  this.keys = this.input.keyboard.createCursorKeys()
 }
 
-function update() {
-  const { keys, mario } = this;
-  const isMarioTouchingFloor = mario.body.touching.down;
-  const isLeftKeyDown = keys.left.isDown;
-  const isRightKeyDown = keys.right.isDown;
-  const isUpKeyDown = keys.up.isDown;
+function update () {
+  const { keys, mario } = this
+  const isMarioTouchingFloor = mario.body.touching.down
+  const isLeftKeyDown = keys.left.isDown
+  const isRightKeyDown = keys.right.isDown
+  const isUpKeyDown = keys.up.isDown
 
-  if (mario.isDead) return;
+  if (mario.isDead) return
 
   if (isLeftKeyDown) {
-    mario.anims.play("mario-walk", true);
-    mario.x -= 2;
-    mario.flipX = true;
+    mario.anims.play('mario-walk', true)
+    mario.x -= 2
+    mario.flipX = true
   } else if (isRightKeyDown) {
-    mario.anims.play("mario-walk", true);
-    mario.x += 2;
-    mario.flipX = false;
+    mario.anims.play('mario-walk', true)
+    mario.x += 2
+    mario.flipX = false
   } else if (isMarioTouchingFloor) {
-    mario.anims.play("mario-idle", true);
+    mario.anims.play('mario-idle', true)
   }
 
   if (isUpKeyDown && isMarioTouchingFloor) {
-    mario.setVelocityY(-300);
-    mario.anims.play("mario-jump", true);
+    mario.setVelocityY(-300)
+    mario.anims.play('mario-jump', true)
   }
 
   if (mario.y >= config.height) {
-    mario.isDead = true;
-    mario.anims.play("mario-dead");
-    mario.setCollideWorldBounds(false);
-    this.sound.add("gameover", { volume: 0.2 }).play();
+    mario.isDead = true
+    mario.anims.play('mario-dead')
+    mario.setCollideWorldBounds(false)
+    this.sound.add('gameover', { volume: 0.2 }).play()
 
     setTimeout(() => {
-      mario.setVelocityY(-350);
-    }, 100);
+      mario.setVelocityY(-350)
+    }, 100)
 
     setTimeout(() => {
-      this.scene.restart();
-    }, 2000);
+      this.scene.restart()
+    }, 2000)
   }
 }
